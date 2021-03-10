95f1b99a5800c41999e273b6bacd53dd85ec1f2d
GunHee Choi <godrjsmgl@gmail.com>
2021-03-10 09:58:50 +0000

    Clear Node js study project

--- study_app/routes/test.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET test listing. */
-router.get('/', function(req, res, next) {
-  res.render('test', { data: 'Gunhee Choi' });
-});
-
-module.exports = router;
